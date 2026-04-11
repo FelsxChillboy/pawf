eb71b25440ac4f3542436ab2f33942cefde3bf7e
FelsxChillboy <azarcoy123@gmail.com>
2026-04-11 11:06:06 +0700

    update

--- project2/app/Controllers/Home.php
+++ project2/app/Controllers/Home.php
@@ -4,8 +4,8 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
+    public function index()
     {
-        return view('welcome_message');
+        return view('v_home'); # mengarahkan ke file v_home.php yang ada di folder Views
     }
 }
