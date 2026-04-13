baa9caf119fd60817260bc692254472eeeee9911
FelsxChillboy <azarcoy123@gmail.com>
2026-04-13 14:58:00 +0700

    update

---
+++ project3/app/Controllers/Post.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class Post extends BaseController
+{
+    public function index()
+    {
+        //buat object
+        $post = new PostModel();
+        /*ambil data dari database*/
+        $data['posts'] = $post->where('status',
+    'published')->findAll();
+
+        //kirim data ke view
+        echo view('post', $data);
+    }
+    //-----------------------------------
+    public function show($slug)
+    {
+    $post = new PostModel();
+        $data['post'] = $post->where([
+        'slug' => $slug,
+        'status' => 'published'
+    ])->first();
+
+    //tamoilan 404 error
+
+    if (!$data['post']) {
+        throw new \CodeIgniter\Exceptions\PageNotFoundException('Post tidak ditemukan');    
+
+    }
+
+    echo view('post_detail', $data);
+    }
+}
